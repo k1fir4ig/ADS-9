6fe6492f6f8a0fc388190eba798d01ac5ad06805
k1fir4ig <maxim140906@gmail.com>
2025-06-09 04:54:37 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -4,27 +4,39 @@
 #include "tree.h"
 
 void printVector(const std::vector<char>& vec) {
-  for (char c : vec) {
-    std::cout << c << " ";
-  }
-  std::cout << std::endl;
+    for (char c : vec) {
+        std::cout << c << " ";
+    }
+    std::cout << std::endl;
 }
 
 int main() {
-  std::vector<char> in = {'1', '2', '3'};
-  PMTree tree(in);
+    std::vector<char> in = {'1', '2', '3'};
+    PMTree tree(in);
 
-  std::cout << "Все перестановки:" << std::endl;
-  auto all_perms = getAllPerms(tree);
-  for (const auto& perm : all_perms) {
-    printVector(perm);
-  }
+    std::cout << "All permutations:" << std::endl;
+    auto all_perms = getAllPerms(tree);
+    for (const auto& perm : all_perms) {
+        printVector(perm);
+    }
 
-  std::cout << "\nПерестановка 1: ";
-  printVector(getPerm1(tree, 1));
+    std::cout << "\nPermutation 1: ";
+    printVector(getPerm1(tree, 1));
 
-  std::cout << "Перестановка 2: ";
-  printVector(getPerm2(tree, 2));
+    std::cout << "Permutation 2: ";
+    printVector(getPerm2(tree, 2));
 
-  return 0;
+    std::cout << "Permutation 3: ";
+    printVector(getPerm2(tree, 3));
+
+    std::cout << "Permutation 4: ";
+    printVector(getPerm2(tree, 4));
+
+    std::cout << "Permutation 5: ";
+    printVector(getPerm2(tree, 5));
+
+    std::cout << "Permutation 6: ";
+    printVector(getPerm2(tree, 6));
+
+    return 0;
 }
