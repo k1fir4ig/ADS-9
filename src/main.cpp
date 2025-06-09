329d65b035f97a31232a23585f77717025045e94
k1fir4ig <maxim140906@gmail.com>
2025-06-09 04:28:08 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -13,18 +13,18 @@ void printVector(const std::vector<char>& vec) {
 int main() {
   std::vector<char> in = {'1', '2', '3'};
   PMTree tree(in);
-    
+
   std::cout << "Все перестановки:" << std::endl;
   auto all_perms = getAllPerms(tree);
   for (const auto& perm : all_perms) {
     printVector(perm);
   }
-    
+
   std::cout << "\nПерестановка 1: ";
   printVector(getPerm1(tree, 1));
-    
+
   std::cout << "Перестановка 2: ";
   printVector(getPerm2(tree, 2));
-    
+
   return 0;
 }
